05f874eae2bf63d497f58667a952e0aabac57390
Scott <scott@scotts-mbp.lan>
2023-03-28 15:59:28 -0700

    overlay interace props

--- components/Overlay.tsx
+++ components/Overlay.tsx
@@ -1,6 +1,10 @@
 import { motion } from "framer-motion";
 
-export default function Overlay({ onClick, children }) {
+interface Props {
+  onClick: () => void;
+  children: React.ReactNode;
+}
+export default function Overlay({ onClick, children }: Props) {
   const overlay = `h-screen w-screen bg-black overflow-x-hidden overflow-y-auto fixed inset-0 z-50 bg-opacity-60 flex`;
   return (
     <motion.div
