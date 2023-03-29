727b21bd1d0a5598399a7179136043d5a95c176e
Scott <scott@scotts-mbp.lan>
2023-03-28 17:08:06 -0700

    api url live, testing headphones

--- app/headphones/page.tsx
+++ app/headphones/page.tsx
@@ -1,26 +1,25 @@
 import PageHero from "@/components/PageHero";
 
 export default async function page() {
-  // const res = await fetch(
-  //   `${process.env.NEXT_PUBLIC_API_ROUTE}/api/headphones`
-  // );
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_ROUTE}/api/headphones`
+  );
 
-  // const data = await res.json();
+  const data = await res.json();
 
-  // const products = data[0].Products;
+  const products = data[0].Products;
   return (
     <>
-      <PageHero title="headphones" />
-      {/* <PageHero title={data[0].name} /> */}
+      <PageHero title={data[0].name} />
 
       <section className="container">
-        {/* {products.map((product: any) => (
+        {products.map((product: any) => (
           <div key={product.id}>
             <h2>{product.name}</h2>
             <p>{product.desc}</p>
             <a href={`/headphones/${product.id}`}>view product</a>
           </div>
-        ))} */}
+        ))}
       </section>
     </>
   );
