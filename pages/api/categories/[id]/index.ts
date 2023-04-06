46ee1d25a62f65bc505297995ce91e752a2e208f
Scott <scott@scotts-mbp.lan>
2023-04-06 10:36:28 -0700

    updating route

--- pages/api/categories/[id]/index.ts
+++ pages/api/categories/[id]/index.ts
@@ -17,6 +17,7 @@ export default async function handler(
       });
       return res.status(200).json(data);
     } catch (error) {
+      console.log("error");
       return res.status(500).json(error);
     }
   }
